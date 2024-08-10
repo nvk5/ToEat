e206d98fc1f55ca5a6b987e37d790bc8593a2f6f
Rustam Tagirov <nvkdev5@gmail.com>
2024-08-10 20:21:58 +0500

    done

---
+++ src/types/IDish.ts
@@ -0,0 +1,5 @@
+export default interface IDish {
+    id: string
+    name: string
+    status: string
+}
---
+++ src/types/IRestaurant.ts
@@ -0,0 +1,7 @@
+export default interface IRestaurant {
+    id: string
+    name: string
+    address: string
+    website: string
+    status: string
+}
