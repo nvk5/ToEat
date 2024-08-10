a1d00a383ff467647b4036b5d5cd4ccada7501ed
Rustam Tagirov <nvkdev5@gmail.com>
2024-08-11 00:25:07 +0500

    script setup

--- src/views/DishesPage.vue
+++ src/views/DishesPage.vue
@@ -1,92 +1,72 @@
-<script lang="ts">
+<script setup lang="ts">
 import NewDishForm from '../components/NewDishForm.vue'
 import DishCard from '../components/DishCard.vue'
 import SideMenu from '../components/SideMenu.vue'
-import { computed, defineComponent, onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import type IDish from '@/types/IDish'
 import { useRoute } from 'vue-router'
 
-export default defineComponent({
-    components: {
-        NewDishForm,
-        DishCard,
-        SideMenu,
+/*
+ * New Dish Form module
+ * */
+const filterText = ref<string>('')
+const showNewForm = ref<boolean>(false)
+const hideForm = () => {
+    showNewForm.value = false
+}
+
+/*
+ * DishList module
+ * */
+const dishList = ref<IDish[]>([
+    {
+        id: '7d9f3f17-964a-4e82-98e5-ecbba4d709a1',
+        name: 'Ghost Pepper Poppers',
+        status: 'Want to Try',
     },
-    setup() {
-        /*
-         * New Dish Form module
-         * */
-        const filterText = ref<string>('')
-        const showNewForm = ref<boolean>(false)
-        const hideForm = () => {
-            showNewForm.value = false
-        }
-
-        /*
-         * DishList module
-         * */
-        const dishList = ref<IDish[]>([
-            {
-                id: '7d9f3f17-964a-4e82-98e5-ecbba4d709a1',
-                name: 'Ghost Pepper Poppers',
-                status: 'Want to Try',
-            },
-            {
-                id: '5c986b74-fa02-4a22-98f2-b1ff3559e85e',
-                name: 'A Little More Chowder Now',
-                status: 'Recommended',
-            },
-            {
-                id: 'c113411d-1589-414f-a283-daf7eedb631e',
-                name: 'Full Laptop Battery',
-                status: 'Do Not Recommend',
-            },
-        ])
-        const filteredDishList = computed<IDish[]>(() => {
-            return dishList.value.filter((dish) => {
-                if (dish.name) {
-                    return dish.name.toLowerCase().includes(filterText.value.toLowerCase())
-                } else {
-                    return dishList.value
-                }
-            })
-        })
-
-        const numberOfDishes = computed<number>(() => {
-            return filteredDishList.value.length
-        })
-        const addDish = (payload: IDish) => {
-            dishList.value.push(payload)
-            hideForm()
-        }
-        const deleteDish = (payload: IDish) => {
-            dishList.value = dishList.value.filter((dish) => {
-                return dish.id !== payload.id
-            })
-        }
-
-        /*
-         * Lifecycle Hooks
-         * */
-
-        onMounted(() => {
-            const route = useRoute()
-            if (route.query.new) {
-                showNewForm.value = true
-            }
-        })
-
-        return {
-            filterText,
-            dishList,
-            showNewForm,
-            filteredDishList,
-            numberOfDishes,
-            hideForm,
-            addDish,
-            deleteDish,
-        }
+    {
+        id: '5c986b74-fa02-4a22-98f2-b1ff3559e85e',
+        name: 'A Little More Chowder Now',
+        status: 'Recommended',
     },
+    {
+        id: 'c113411d-1589-414f-a283-daf7eedb631e',
+        name: 'Full Laptop Battery',
+        status: 'Do Not Recommend',
+    },
+])
+const filteredDishList = computed<IDish[]>(() => {
+    return dishList.value.filter((dish) => {
+        if (dish.name) {
+            return dish.name.toLowerCase().includes(filterText.value.toLowerCase())
+        } else {
+            return dishList.value
+        }
+    })
+})
+
+const numberOfDishes = computed<number>(() => {
+    return filteredDishList.value.length
+})
+const addDish = (payload: IDish) => {
+    dishList.value.push(payload)
+    hideForm()
+}
+const deleteDish = (payload: IDish) => {
+    dishList.value = dishList.value.filter((dish) => {
+        return dish.id !== payload.id
+    })
+}
+
+/*
+ * Lifecycle Hooks
+ * */
+
+onMounted(() => {
+    const route = useRoute()
+    if (route.query.new) {
+        showNewForm.value = true
+    }
 })
 </script>
 
--- src/views/RestaurantsPage.vue
+++ src/views/RestaurantsPage.vue
@@ -1,97 +1,77 @@
-<script lang="ts">
+<script setup lang="ts">
 import NewRestaurantForm from '../components/NewRestaurantForm.vue'
 import RestaurantCard from '../components/RestaurantCard.vue'
 import SideMenu from '../components/SideMenu.vue'
-import { computed, defineComponent, onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import type IRestaurant from '@/types/IRestaurant'
 import { useRoute } from 'vue-router'
 
-export default defineComponent({
-    components: {
-        NewRestaurantForm,
-        RestaurantCard,
-        SideMenu,
+/*
+ * New Restaurant Form Module
+ * */
+const filterText = ref<string>('')
+const showNewForm = ref<boolean>(false)
+const hideForm = () => {
+    showNewForm.value = false
+}
+
+/*
+ * Restaurant List Module
+ * */
+const restaurantList = ref<IRestaurant[]>([
+    {
+        id: '9f995ce4-d2fc-4d00-af1d-6cb1647c6bd3',
+        name: 'Quiche From a Rose',
+        address: '283 Thisisnota St.',
+        website: 'www.quichefromarose.com',
+        status: 'Want to Try',
     },
-    setup() {
-        /*
-         * New Restaurant Form Module
-         * */
-        const filterText = ref<string>('')
-        const showNewForm = ref<boolean>(false)
-        const hideForm = () => {
-            showNewForm.value = false
-        }
-
-        /*
-         * Restaurant List Module
-         * */
-        const restaurantList = ref<IRestaurant[]>([
-            {
-                id: '9f995ce4-d2fc-4d00-af1d-6cb1647c6bd3',
-                name: 'Quiche From a Rose',
-                address: '283 Thisisnota St.',
-                website: 'www.quichefromarose.com',
-                status: 'Want to Try',
-            },
-            {
-                id: 'ae62a3da-791b-4f44-99a1-4be1b0ec30b8',
-                name: 'Tamago Never Dies',
-                address: '529 Letsgofora Dr.',
-                website: 'www.tamagoneverdies.com',
-                status: 'Recommended',
-            },
-            {
-                id: '9b361dae-2d44-4499-9940-97e188d41a32',
-                name: 'Penne For Your Thoughts',
-                address: '870 Thisisa St.',
-                website: 'www.penneforyourthoughts.com',
-                status: 'Do Not Recommend',
-            },
-        ])
-        const filteredRestaurantList = computed<IRestaurant[]>(() => {
-            return restaurantList.value.filter((restaurant) => {
-                if (restaurant.name) {
-                    return restaurant.name.toLowerCase().includes(filterText.value.toLowerCase())
-                } else {
-                    return restaurantList.value
-                }
-            })
-        })
-        const numberOfRestaurants = computed<number>(() => {
-            return filteredRestaurantList.value.length
-        })
-        const addRestaurant = (payload: IRestaurant) => {
-            restaurantList.value.push(payload)
-            hideForm()
-        }
-        const deleteRestaurant = (payload: IRestaurant) => {
-            restaurantList.value = restaurantList.value.filter((restaurant: IRestaurant) => {
-                return restaurant.id !== payload.id
-            })
-        }
-
-        /*
-         * Lifecycle Hooks
-         * */
-
-        onMounted(() => {
-            const route = useRoute()
-            if (route.query.new) {
-                showNewForm.value = true
-            }
-        })
-
-        return {
-            filterText,
-            restaurantList,
-            showNewForm,
-            filteredRestaurantList,
-            numberOfRestaurants,
-            addRestaurant,
-            deleteRestaurant,
-            hideForm,
-        }
+    {
+        id: 'ae62a3da-791b-4f44-99a1-4be1b0ec30b8',
+        name: 'Tamago Never Dies',
+        address: '529 Letsgofora Dr.',
+        website: 'www.tamagoneverdies.com',
+        status: 'Recommended',
     },
+    {
+        id: '9b361dae-2d44-4499-9940-97e188d41a32',
+        name: 'Penne For Your Thoughts',
+        address: '870 Thisisa St.',
+        website: 'www.penneforyourthoughts.com',
+        status: 'Do Not Recommend',
+    },
+])
+const filteredRestaurantList = computed<IRestaurant[]>(() => {
+    return restaurantList.value.filter((restaurant) => {
+        if (restaurant.name) {
+            return restaurant.name.toLowerCase().includes(filterText.value.toLowerCase())
+        } else {
+            return restaurantList.value
+        }
+    })
+})
+const numberOfRestaurants = computed<number>(() => {
+    return filteredRestaurantList.value.length
+})
+const addRestaurant = (payload: IRestaurant) => {
+    restaurantList.value.push(payload)
+    hideForm()
+}
+const deleteRestaurant = (payload: IRestaurant) => {
+    restaurantList.value = restaurantList.value.filter((restaurant: IRestaurant) => {
+        return restaurant.id !== payload.id
+    })
+}
+
+/*
+ * Lifecycle Hooks
+ * */
+
+onMounted(() => {
+    const route = useRoute()
+    if (route.query.new) {
+        showNewForm.value = true
+    }
 })
 </script>
 
